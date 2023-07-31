tudo-seal/clsp-python

separated benchmarks and tests, added unittest framework

tests/test_K.py
@@ -1,0 +1,101 @@
+from collections.abc import Callable, Mapping
+import logging
+import unittest
+from cls import (
+    Type,
+    Constructor,
+    Arrow,
+    FiniteCombinatoryLogic,
+    enumerate_terms,
+    Subtypes,
+)
+from cls.enumeration import interpret_term
+
+X: str = "X"
+Y: str = "Y"
+K: Callable[[str], Callable[[str], str]] = lambda x: (lambda y: x)
+
+
+def K2(x: str, y: str) -> str:
+    return x
+
+
+MAP: Callable[[str, Callable[[str], str]], str] = lambda x, f: f(x)
+
+
+def MAP2(x: str, f: Callable[[str], str]) -> str:
+    return f(x)
+
+
+class TestK(unittest.TestCase):
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(
+        format="%(module)s %(levelname)s: %(message)s",
+        # level=logging.INFO,
+    )
+
+    def setUp(self) -> None:
+        a: Type = Constructor("a")
+        b: Type = Constructor("b")
+        c: Type = Constructor("c")
+        # d: Type = Constructor("d")
+
+        repository: Mapping[
+            str
+            | Callable[[str, str], str]
+            | Callable[[str], Callable[[str], str]]
+            | Callable[[str, Callable[[str], str]], str],
+            Type,
+        ] = dict(
+            {
+                X: a,
+                Y: b,
+                K: Arrow(a, Arrow(b, c)),
+                K2: Arrow(a, Arrow(b, c)),
+                MAP: Arrow(b, Arrow(Arrow(b, c), c)),
+                MAP2: Arrow(b, Arrow(Arrow(b, c), c)),
+            }
+        )
+        environment: dict[str, set[str]] = dict()
+        subtypes = Subtypes(environment)
+
+        target = c
+        # target = Var(Arrow(b, c))
+
+        fcl = FiniteCombinatoryLogic(repository, subtypes)
+        result = fcl.inhabit(target)
+
+        self.enumerated_result = list(enumerate_terms(target, result))
+
+        self.interpreted_terms = []
+        for real_result in self.enumerated_result:
+            self.logger.info(real_result)
+            interpreted_term = interpret_term(real_result)
+            self.interpreted_terms.append(interpreted_term)
+            self.assertEqual("X", interpreted_term)
+            self.logger.info(interpreted_term)
+
+    def test_length(self):
+        self.assertEqual(6, len(self.enumerated_result))
+
+    def test_terms(self):
+        self.assertIn((K, (("X", ()), ("Y", ()))), self.enumerated_result[:2])
+        self.assertIn((K2, (("X", ()), ("Y", ()))), self.enumerated_result[:2])
+        self.assertIn((MAP, (("Y", ()), (K, (("X", ()),)))), self.enumerated_result[2:])
+        self.assertIn(
+            (MAP2, (("Y", ()), (K, (("X", ()),)))), self.enumerated_result[2:]
+        )
+        self.assertIn(
+            (MAP, (("Y", ()), (K2, (("X", ()),)))), self.enumerated_result[2:]
+        )
+        self.assertIn(
+            (MAP2, (("Y", ()), (K2, (("X", ()),)))), self.enumerated_result[2:]
+        )
+
+    def test_interpretations(self):
+        for x in self.interpreted_terms:
+            self.assertEqual("X", x)
+
+
+if __name__ == "__main__":
+    unittest.main()

tests/test_list.py
@@ -1,0 +1,71 @@
+import logging
+from typing import Any, TypeVar
+import unittest
+from cls import inhabit_and_interpret
+from cls.types import Arrow, Constructor, Type
+
+T = TypeVar("T")
+
+
+def nil() -> list[Any]:
+    return list()
+
+
+def cons(x: T, xs: list[T]) -> list[T]:
+    the_list = [x]
+    the_list.extend(xs)
+    return the_list
+
+
+A = Constructor("A")
+B = Constructor("B")
+C = Constructor("C")
+
+
+def List(a: Type) -> Constructor:
+    return Constructor("List", a)
+
+
+def a_to_b(a: list[Any]) -> int:
+    return len(a)
+
+
+exampleRepo = {
+    nil: List(A),
+    cons: Arrow(A, Arrow(List(A), List(A))),
+    a_to_b: Arrow(A, B),
+    lambda _: "b": Arrow(A, B),
+    lambda _: 2: Arrow(A, B),
+    lambda f, xs: list(map(f, xs)): Arrow(Arrow(A, B), Arrow(List(A), List(B))),
+    "a": A,
+}
+
+
+class TestList(unittest.TestCase):
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(
+        format="%(module)s %(levelname)s: %(message)s",
+        # level=logging.INFO,
+    )
+
+    def setUp(self) -> None:
+        self.results = list(inhabit_and_interpret(exampleRepo, List(B)))
+        for z in self.results:
+            self.logger.info(z)
+
+    def test_size(self) -> None:
+        self.assertEqual(100, len(self.results))
+
+    def test_all_monotone(self) -> None:
+        max_len = 0
+        for z in self.results:
+            self.assertGreaterEqual(len(z), max_len)
+            max_len = len(z)
+
+    def test_all_lists(self) -> None:
+        for z in self.results:
+            self.assertIsInstance(z, list)
+
+
+if __name__ == "__main__":
+    unittest.main()

tests/test_wrong_spec.py
@@ -1,0 +1,38 @@
+from collections.abc import Callable
+import logging
+import unittest
+from cls import Type, Constructor, Arrow, Intersection, inhabit_and_interpret
+
+
+class TestWrongSpec2(unittest.TestCase):
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(
+        format="%(module)s %(levelname)s: %(message)s",
+        # level=logging.INFO,
+    )
+
+    def test_wrong_spec(self) -> None:
+        a: Type = Constructor("a")
+        b: Type = Constructor("b")
+        c: Type = Constructor("c")
+        d: Type = Constructor("d")
+
+        X: str = "X"
+        Y: str = "Y"
+        F: Callable[[str], str] = lambda x: f"F({x})"
+
+        repository: dict[str | Callable[[str], str], Type] = dict(
+            {
+                X: Intersection(Intersection(a, b), d),
+                Y: Arrow(a, b),
+                F: Intersection(Arrow(a, b), Arrow(d, Intersection(a, c))),
+            }
+        )
+
+        with self.assertRaises(RuntimeError):
+            for real_result in inhabit_and_interpret(repository, b):
+                self.logger.info(real_result)
+
+
+if __name__ == "__main__":
+    unittest.main()

tudo-seal/bcls-python

tests/test_K.py
@@ -1,0 +1,101 @@
+from collections.abc import Callable, Mapping
+import logging
+import unittest
+from bcls import (
+    Type,
+    Constructor,
+    Arrow,
+    FiniteCombinatoryLogic,
+    enumerate_terms,
+    Subtypes,
+)
+from bcls.enumeration import interpret_term
+
+X: str = "X"
+Y: str = "Y"
+K: Callable[[str], Callable[[str], str]] = lambda x: (lambda y: x)
+
+
+def K2(x: str, y: str) -> str:
+    return x
+
+
+MAP: Callable[[str, Callable[[str], str]], str] = lambda x, f: f(x)
+
+
+def MAP2(x: str, f: Callable[[str], str]) -> str:
+    return f(x)
+
+
+class TestK(unittest.TestCase):
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(
+        format="%(module)s %(levelname)s: %(message)s",
+        # level=logging.INFO,
+    )
+
+    def setUp(self) -> None:
+        a: Type = Constructor("a")
+        b: Type = Constructor("b")
+        c: Type = Constructor("c")
+        # d: Type = Constructor("d")
+
+        repository: Mapping[
+            str
+            | Callable[[str, str], str]
+            | Callable[[str], Callable[[str], str]]
+            | Callable[[str, Callable[[str], str]], str],
+            Type,
+        ] = dict(
+            {
+                X: a,
+                Y: b,
+                K: Arrow(a, Arrow(b, c)),
+                K2: Arrow(a, Arrow(b, c)),
+                MAP: Arrow(b, Arrow(Arrow(b, c), c)),
+                MAP2: Arrow(b, Arrow(Arrow(b, c), c)),
+            }
+        )
+        environment: dict[str, set[str]] = dict()
+        subtypes = Subtypes(environment)
+
+        target = c
+        # target = Var(Arrow(b, c))
+
+        fcl = FiniteCombinatoryLogic(repository, subtypes)
+        result = fcl.inhabit(target)
+
+        self.enumerated_result = list(enumerate_terms(target, result))
+
+        self.interpreted_terms = []
+        for real_result in self.enumerated_result:
+            self.logger.info(real_result)
+            interpreted_term = interpret_term(real_result)
+            self.interpreted_terms.append(interpreted_term)
+            self.assertEqual("X", interpreted_term)
+            self.logger.info(interpreted_term)
+
+    def test_length(self) -> None:
+        self.assertEqual(6, len(self.enumerated_result))
+
+    def test_terms(self) -> None:
+        self.assertIn((K, (("X", ()), ("Y", ()))), self.enumerated_result[:2])
+        self.assertIn((K2, (("X", ()), ("Y", ()))), self.enumerated_result[:2])
+        self.assertIn((MAP, (("Y", ()), (K, (("X", ()),)))), self.enumerated_result[2:])
+        self.assertIn(
+            (MAP2, (("Y", ()), (K, (("X", ()),)))), self.enumerated_result[2:]
+        )
+        self.assertIn(
+            (MAP, (("Y", ()), (K2, (("X", ()),)))), self.enumerated_result[2:]
+        )
+        self.assertIn(
+            (MAP2, (("Y", ()), (K2, (("X", ()),)))), self.enumerated_result[2:]
+        )
+
+    def test_interpretations(self) -> None:
+        for x in self.interpreted_terms:
+            self.assertEqual("X", x)
+
+
+if __name__ == "__main__":
+    unittest.main()

tests/test_list.py
@@ -1,0 +1,71 @@
+import logging
+from typing import Any, TypeVar
+import unittest
+from bcls import inhabit_and_interpret
+from bcls.types import Arrow, Constructor, Type
+
+T = TypeVar("T")
+
+
+def nil() -> list[Any]:
+    return list()
+
+
+def cons(x: T, xs: list[T]) -> list[T]:
+    the_list = [x]
+    the_list.extend(xs)
+    return the_list
+
+
+A = Constructor("A")
+B = Constructor("B")
+C = Constructor("C")
+
+
+def List(a: Type) -> Constructor:
+    return Constructor("List", a)
+
+
+def a_to_b(a: list[Any]) -> int:
+    return len(a)
+
+
+exampleRepo = {
+    nil: List(A),
+    cons: Arrow(A, Arrow(List(A), List(A))),
+    a_to_b: Arrow(A, B),
+    lambda _: "b": Arrow(A, B),
+    lambda _: 2: Arrow(A, B),
+    lambda f, xs: list(map(f, xs)): Arrow(Arrow(A, B), Arrow(List(A), List(B))),
+    "a": A,
+}
+
+
+class TestList(unittest.TestCase):
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(
+        format="%(module)s %(levelname)s: %(message)s",
+        # level=logging.INFO,
+    )
+
+    def setUp(self) -> None:
+        self.results = list(inhabit_and_interpret(exampleRepo, List(B)))
+        for z in self.results:
+            self.logger.info(z)
+
+    def test_size(self) -> None:
+        self.assertEqual(100, len(self.results))
+
+    def test_all_monotone(self) -> None:
+        max_len = 0
+        for z in self.results:
+            self.assertGreaterEqual(len(z), max_len)
+            max_len = len(z)
+
+    def test_all_lists(self) -> None:
+        for z in self.results:
+            self.assertIsInstance(z, list)
+
+
+if __name__ == "__main__":
+    unittest.main()

tests/test_wrong_spec.py
@@ -1,0 +1,38 @@
+from collections.abc import Callable
+import logging
+import unittest
+from bcls import Type, Constructor, Arrow, Intersection, inhabit_and_interpret
+
+
+class TestWrongSpec2(unittest.TestCase):
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(
+        format="%(module)s %(levelname)s: %(message)s",
+        # level=logging.INFO,
+    )
+
+    def test_wrong_spec(self) -> None:
+        a: Type = Constructor("a")
+        b: Type = Constructor("b")
+        c: Type = Constructor("c")
+        d: Type = Constructor("d")
+
+        X: str = "X"
+        Y: str = "Y"
+        F: Callable[[str], str] = lambda x: f"F({x})"
+
+        repository: dict[str | Callable[[str], str], Type] = dict(
+            {
+                X: Intersection(Intersection(a, b), d),
+                Y: Arrow(a, b),
+                F: Intersection(Arrow(a, b), Arrow(d, Intersection(a, c))),
+            }
+        )
+
+        with self.assertRaises(RuntimeError):
+            for real_result in inhabit_and_interpret(repository, b):
+                self.logger.info(real_result)
+
+
+if __name__ == "__main__":
+    unittest.main()